3fbb5ef2126878046c97be77eeab0f9e9a8feb96
peterhuszti <peterhuszti4@gmail.com>
2016-04-20 12:24:29 +0200

    ctr, getter, setter

--- haskell/mtx.hs
+++ haskell/mtx.hs
@@ -26,13 +26,25 @@ data Matrix = M
   }
 
 newMatrix :: Int -> Matrix
-newMatrix n = (M
+newMatrix n = M
   { mDat = (Seq.replicate (n*n) 0)
   , mSize = n
-  , mIx = (\(a,b) -> if a>n || b>n then Nothing
-                     else Just (Seq.index mDat ((n-1)*a+b))
+  , mIx = (\(i,j) -> if i>n || j>n || i<1 || j<1 then Nothing
+                     else Just (n*(i-1)+(j-1))
           )
-  })
+  }
+  
+setMatrix :: (Int, Int) -> Integer -> Matrix -> Matrix
+setMatrix (i,j) x mtx = do
+    if mIx mtx (i,j) == Nothing
+    then mtx
+    else M { mDat = Seq.update (fromJust (mIx mtx (i,j))) x (mDat mtx), mSize = mSize mtx, mIx = mIx mtx }
+
+getMatrix :: (Int, Int) -> Matrix -> Maybe Integer
+getMatrix (i,j) mtx = do
+    if mIx mtx (i,j) == Nothing
+    then Nothing
+    else Just(Seq.index (mDat mtx) (fromJust $ mIx mtx (i,j)))
   
 test_newMatrix :: [Bool]
 test_newMatrix =
@@ -45,5 +57,34 @@ test_newMatrix =
   , mIx m3 (3, 2) == Just 7
   ]
   
+test_setMatrix :: [Bool]
+test_setMatrix =
+  let m2 = newMatrix 2 in
+  [ mDat (setMatrix (0, 0) 1 m2) == mDat m2
+  , mDat (setMatrix (1, 1) 1 m2) == Seq.fromList [1, 0, 0, 0]
+  , mDat (setMatrix (1, 2) 1 m2) == Seq.fromList [0, 1, 0, 0]
+  ]
+
+test_getMatrix :: [Bool]
+test_getMatrix =
+  let m2 = setMatrix (1, 2) 1 $ newMatrix 2 in
+  [ getMatrix (0, 0) m2 == Nothing
+  , getMatrix (1, 1) m2 == Just 0
+  , getMatrix (1, 2) m2 == Just 1
+  ]
+  
 main = do
     print test_newMatrix
+    print test_setMatrix
+    print test_getMatrix
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
